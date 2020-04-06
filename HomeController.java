80eeb8847f7973f1078423ccce3d020ba7e3ee38
ucceoum <ucceoum@gmail.com>
2020-04-06 09:54:41 +0900

    a

--- HomeController.java
+++ HomeController.java
@@ -23,32 +23,32 @@ import lombok.extern.slf4j.Slf4j;
 @Controller
 @Slf4j
 public class HomeController {
-	
+
 	@RequestMapping(value = "/", method = RequestMethod.GET)
 	public String home() {
 //		log.info("index 페이지 요청");
-		
+
 		return "index";
 	}
 	@GetMapping("/umbrella")
 	public String umbrella() {
 //		log.info("umbrella 페이지 요청");
-		
+
 		return "umbrella2";
 	}
-	
+
 	@PostMapping("/umbrella/save_center")
 	public void umbrella_save_center(String center_lat, String center_lng, String user_uuid) {
-		
-		File file = new File("e:\\"+user_uuid+"center.txt");
+
+		File file = new File("d:\\"+user_uuid+"center.txt");
 		FileWriter writer = null;
-		
+
 		try {
 			writer = new FileWriter(file, false);
 			writer.write(center_lat+" "+center_lng);
 			writer.flush();
-			
-			
+
+
 		} catch (Exception e) {
 			e.printStackTrace();
 		}finally {
@@ -62,39 +62,39 @@ public class HomeController {
 		}
 		System.out.println("saveCenter_done");
 	}
-	
+
 	@PostMapping("/umbrella/get_center")
 	@ResponseBody
 	public String umbrella_get_center(String user_uuid) {
 		String latlng = "";
-		File file = new File("e:\\"+user_uuid+"center.txt");
+		File file = new File("d:\\"+user_uuid+"center.txt");
 		try(FileReader filereader = new FileReader(file);
 			BufferedReader br = new BufferedReader(filereader)
 			) {
 			latlng = br.readLine();
-			
-			
+
+
 		} catch (Exception e) {
 			e.printStackTrace();
 		}
-		
+
 
 		System.out.println("getCenter_done"+latlng);
 		return latlng;
 	}
 
-	
+
 	@PostMapping("/umbrella/save_level")
 	public void umbrella_save_level(String level, String user_uuid) {
-		
-		File file = new File("e:\\"+user_uuid+"level.txt");
+
+		File file = new File("d:\\"+user_uuid+"level.txt");
 		FileWriter writer = null;
-		
+
 		try {
 			writer = new FileWriter(file, false);
 			writer.write(level);
 			writer.flush();
-			
+
 			System.out.println("done");
 		} catch (Exception e) {
 			e.printStackTrace();
@@ -107,20 +107,20 @@ public class HomeController {
 				e2.printStackTrace();
 			}
 		}
-		
+
 	}
-	
+
 	@PostMapping("/umbrella/get_level")
 	@ResponseBody
 	public String umbrella_get_level(String user_uuid) {
 		String level = "";
-		File file = new File("e:\\"+user_uuid+"level.txt");
+		File file = new File("d:\\"+user_uuid+"level.txt");
 		try(FileReader filereader = new FileReader(file);
 			BufferedReader br = new BufferedReader(filereader)
 			) {
 			level = br.readLine();
 			System.out.println("get_level1 : "+level);
-			
+
 		} catch (Exception e) {
 			e.printStackTrace();
 		}
